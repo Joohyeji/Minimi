75eb7bd0e5a61d68bf5e7dfd16c5e43b16307fb2
Joohyeji <rosyjoo1999@gmail.com>
2024-07-16 20:28:59 +0900

    💄 회원가입 페이지 색상 수정

--- src/renderer/src/components/Welcome/SignIn.jsx
+++ src/renderer/src/components/Welcome/SignIn.jsx
@@ -5,10 +5,10 @@ import Input from './Input'
 
 function SignIn() {
   return (
-    <div className="flex flex-col justify-between justify-center w-full h-full items-center mt-10 gap-10 px-24">
+    <div className="flex flex-col justify-between justify-center w-full h-full items-center gap-10 px-24">
       <h1 className="text-5xl font-black">Create new Account</h1>
       <GoogleBtn />
-      <p className="text-gray-500">or</p>
+      <p className="text-neutral-500">or</p>
       <form className="w-5/12 min-w-96 flex flex-col justify-between justify-center gap-4">
         <Input />
         <Input />
