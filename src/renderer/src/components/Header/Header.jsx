c82d0c93c86fc076863ca57ecede111e7813595c
Joohyeji <rosyjoo1999@gmail.com>
2024-07-16 20:27:43 +0900

    💄 헤더 ui 수정 - z-index 추가

--- src/renderer/src/components/Header/Header.jsx
+++ src/renderer/src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import UserInfo from './UserInfo'
 
 function Header() {
   return (
-    <header className="h-20 flex justify-between items-center backdrop-blur-sm bg-white/30">
-      <img src={homeLogo} alt="logo" className="h-3/4" />
+    <header className="h-20 flex justify-between items-center backdrop-blur-sm bg-white/30 sticky top-0 z-10">
+      <img src={homeLogo} alt="logo" className="h-full" />
       <Search />
       <UserInfo />
     </header>
