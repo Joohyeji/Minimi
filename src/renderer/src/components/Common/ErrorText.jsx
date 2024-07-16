86846d702e8f332a3f06728e50580396364a51fe
Joohyeji <rosyjoo1999@gmail.com>
2024-07-16 16:04:03 +0900

    ✨ 에러 메세지 정적화면 구현

---
+++ src/renderer/src/components/Common/ErrorText.jsx
@@ -0,0 +1,5 @@
+function ErrorText() {
+  return <p className="mt-1 px-3 text-red-500 text-sm">The password is different.</p>
+}
+
+export default ErrorText
--- src/renderer/src/components/Welcome/Input.jsx
+++ src/renderer/src/components/Welcome/Input.jsx
@@ -1,10 +1,15 @@
+import ErrorText from '../Common/ErrorText'
+
 function Input() {
   return (
-    <input
-      type="text"
-      placeholder="Enter email address"
-      className="w-full h-12 bg-gray-100 rounded-xl px-4 py-2 focus:outline-none focus:ring-1 focus:ring-gray-300"
-    />
+    <div>
+      <input
+        type="text"
+        placeholder="Enter email address"
+        className="w-full h-12 bg-gray-100 rounded-xl px-4 py-2 focus:outline-none focus:ring-1 focus:ring-gray-300"
+      />
+      {/* <ErrorText /> */}
+    </div>
   )
 }
 
