2fb8ed8bb6b90a1861223f260c3e4c880f6102aa
Joohyeji <rosyjoo1999@gmail.com>
2024-07-16 20:41:55 +0900

    ✨ Minimi추가 카드 정적화면 구현

---
+++ src/renderer/src/components/Dashboard/CreateMinimiCard.jsx
@@ -0,0 +1,9 @@
+function CreateMinimiCard() {
+  return (
+    <article className="h-[230px] border border-slate-50 rounded-3xl relative shadow-lg hover:shadow-md cursor-pointer overflow-hidden p-5 flex items-center justify-center  hover:bg-neutral-900 text-neutral-500 hover:text-minimi-green">
+      <p className="font-bold text-5xl">+</p>
+    </article>
+  )
+}
+
+export default CreateMinimiCard
--- src/renderer/src/components/Dashboard/MyMinimies.jsx
+++ src/renderer/src/components/Dashboard/MyMinimies.jsx
@@ -1,10 +1,12 @@
 import MinimiCard from './MinimiCard'
+import CreateMinimiCard from './CreateMinimiCard'
 
 function MyMinimies() {
   return (
     <div className="mt-10 grid grid-cols-5 gap-12 gap-y-16 overflow-auto py-5 h-[564px]">
       <MinimiCard />
       <MinimiCard />
+      <CreateMinimiCard />
     </div>
   )
 }
