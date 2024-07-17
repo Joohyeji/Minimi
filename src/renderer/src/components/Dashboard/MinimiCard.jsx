1d84cf5471c70ba3d20160deb0e0351a3c0a7a1e
Joohyeji <rosyjoo1999@gmail.com>
2024-07-17 15:04:51 +0900

    💄 Minimi 카드 닉네임 추가

--- src/renderer/src/components/Dashboard/MinimiCard.jsx
+++ src/renderer/src/components/Dashboard/MinimiCard.jsx
@@ -2,6 +2,9 @@ function MinimiCard() {
   return (
     <article className="h-[230px] border border-slate-50 rounded-3xl relative shadow-lg hover:shadow-md cursor-pointer overflow-hidden p-5">
       <div className="w-full absolute left-0 top-0 bg-minimi-green h-[20px]"></div>
+      <div className="flex flex-row-reverse mt-2 text-neutral-500 font-medium">
+        <p>nickname</p>
+      </div>
       <div className="absolute bottom-5">
         <p className="font-black text-lg">Title</p>
         <p className="font-medium text-sm text-neutral-500">latitute, latitute</p>
