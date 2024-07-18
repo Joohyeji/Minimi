23781b124c5008a7b050126ebe1795ff8ca3064c
Joohyeji <rosyjoo1999@gmail.com>
2024-07-19 01:55:51 +0900

    💄 카드 ui 수정

--- src/renderer/src/components/CreateMinimi/SettingCard.jsx
+++ src/renderer/src/components/CreateMinimi/SettingCard.jsx
@@ -1,6 +1,6 @@
 function SettingCard() {
   return (
-    <button className="bg-white drop-shadow-md border border-slate-50 rounded-3xl p-3 text-center text-neutral-900 font-bold text-md hover:shadow-inner hover:drop-shadow-sm">
+    <button className="bg-white drop-shadow-md border border-slate-50 rounded-3xl p-2 text-center text-neutral-900 font-medium text-md hover:shadow-inner hover:drop-shadow-sm">
       <p>Brightenss</p>
     </button>
   )
--- src/renderer/src/components/CreateMinimi/SettingInput.jsx
+++ src/renderer/src/components/CreateMinimi/SettingInput.jsx
@@ -6,12 +6,12 @@ function SettingInput() {
   return (
     <div>
       <div className="flex items-center gap-10">
-        <div className="flex w-10/12 bg-white drop-shadow-md border border-slate-50 rounded-full p-5 px-10 text-neutral-900 font-bold  text-2xl gap-5">
+        <div className="flex w-10/12 bg-white drop-shadow-md border border-slate-50 rounded-full p-3 px-10 text-neutral-900 font-bold  text-2xl gap-5">
           <span></span>
           <input
             type="text"
             placeholder="Write this minimi's name"
-            className="w-full border-b-2 outline-none font-black  focus:border-black"
+            className="w-full border-b-2 outline-none font-bold  focus:border-black"
           />
         </div>
         <button className="h-10 hover:rotate-90">
