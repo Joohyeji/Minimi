ebcce2592d8991ca14025767d47e611afd6de912
Joohyeji <rosyjoo1999@gmail.com>
2024-07-18 23:26:10 +0900

    🐛 로그아웃 버그 픽스

--- src/renderer/src/components/Header/Header.jsx
+++ src/renderer/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import UserInfo from './UserInfo'
 
 function Header() {
   const { user, isLoggedIn } = useAuthStore()
-  const userName = user.displayName
+  const userName = isLoggedIn ? user.displayName : ''
 
   return (
     <header className="h-20 flex justify-between items-center backdrop-blur-sm bg-white/30 sticky top-0 z-10">
