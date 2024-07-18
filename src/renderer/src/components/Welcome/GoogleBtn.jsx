e76d39c69fc946650eb9e8cc06133067c199b463
Joohyeji <rosyjoo1999@gmail.com>
2024-07-18 16:16:25 +0900

    ✨ 구글 회원가입 구현

--- src/renderer/src/components/Welcome/GoogleBtn.jsx
+++ src/renderer/src/components/Welcome/GoogleBtn.jsx
@@ -1,8 +1,22 @@
+import { signInWithPopup } from 'firebase/auth'
+import { auth, googleProvider } from '../../firebase'
+
 import googleIcon from '../../assets/img/google_icon.png'
 
 function GoogleBtn() {
+  const handleGoogleBtnClick = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider)
+    } catch (err) {
+      console.log('Error during Google Sign-In:', err)
+    }
+  }
+
   return (
-    <button className="flex justify-center border rounded-2xl py-3 px-5 w-5/12 min-w-96 text-gray-700 h-12 items-center hover:bg-gray-100">
+    <button
+      onClick={handleGoogleBtnClick}
+      className="flex justify-center border rounded-2xl py-3 px-5 w-5/12 min-w-96 text-gray-700 h-12 items-center hover:bg-gray-100"
+    >
       <div className="w-5 mx-4">
         <img src={googleIcon}></img>
       </div>
