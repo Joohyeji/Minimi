54841c3a66b4f4e64529e2f4332c30fd6e8b0d95
Joohyeji <rosyjoo1999@gmail.com>
2024-07-18 23:13:47 +0900

    ✨ 로그인 UI 구현 - 닉네임 표시, 검색창 표시

--- src/renderer/src/components/Header/Header.jsx
+++ src/renderer/src/components/Header/Header.jsx
@@ -1,13 +1,22 @@
+import useAuthStore from '../../store/useAuthStore'
+
 import homeLogo from '../../assets/img/home_logo.svg'
 import Search from './Search'
 import UserInfo from './UserInfo'
 
 function Header() {
+  const { user, isLoggedIn } = useAuthStore()
+  const userName = user.displayName
+
   return (
     <header className="h-20 flex justify-between items-center backdrop-blur-sm bg-white/30 sticky top-0 z-10">
       <img src={homeLogo} alt="logo" className="h-full" />
-      <Search />
-      <UserInfo />
+      {isLoggedIn && (
+        <>
+          <Search />
+          <UserInfo userName={userName} />
+        </>
+      )}
     </header>
   )
 }
--- src/renderer/src/components/Header/UserInfo.jsx
+++ src/renderer/src/components/Header/UserInfo.jsx
@@ -1,23 +1,35 @@
+import PropTypes from 'prop-types'
 import { useNavigate } from 'react-router-dom'
+import { auth } from '../../firebase'
+
 import logoutIcon from '../../assets/img/logout_icon.png'
 
-function UserInfo() {
+function UserInfo({ userName }) {
   const navigate = useNavigate()
 
-  const handleLogOutClick = () => {
-    navigate('/')
+  const handleLogOutClick = async () => {
+    try {
+      await auth.signOut()
+      navigate('/')
+    } catch (err) {
+      alert('로그아웃에 실패했습니다.')
+    }
   }
 
   return (
     <div className="flex items-center">
       <span className="text-xl mr-8">
-        Hello, <b className="tracking-wider">Hyeji</b>
+        Hello, <b className="tracking-wider">{userName}</b>
       </span>
       <button onClick={handleLogOutClick}>
         <img className="cursor-pointer" src={logoutIcon} />
       </button>
     </div>
   )
+}
+
+UserInfo.propTypes = {
+  userName: PropTypes.string
 }
 
 export default UserInfo
