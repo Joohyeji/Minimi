0c13a15c019592c0ca0bfc8acc437dc301f38de9
Joohyeji <rosyjoo1999@gmail.com>
2024-07-24 14:00:31 +0900

    🐛 wallpaper firebase 버그 수정

--- src/renderer/src/hooks/useUpload.jsx
+++ src/renderer/src/hooks/useUpload.jsx
@@ -1,6 +1,10 @@
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 
 const uploadWallpaperToFirebase = async (file) => {
+  if (typeof file === 'string' && file.startsWith('https://')) {
+    return file
+  }
+
   const storage = getStorage()
   const storageRef = ref(storage, `wallpapers/${file.name}`)
 
