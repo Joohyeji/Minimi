50c81e69f33da0910434f3c7209b23961d41c7a8
Joohyeji <rosyjoo1999@gmail.com>
2024-07-30 14:48:02 +0900

    ✨ dashboard에서만 검색할 수 있도록 수정

--- src/renderer/src/components/Header/Header.jsx
+++ src/renderer/src/components/Header/Header.jsx
@@ -1,19 +1,22 @@
+import { useLocation } from 'react-router-dom'
 import useAuthStore from '../../store/useAuthStore'
 
-import homeLogo from '../../assets/img/home_logo.svg'
 import Search from './Search'
 import UserInfo from './UserInfo'
+import homeLogo from '../../assets/img/home_logo.svg'
 
 function Header() {
   const { user, isLoggedIn } = useAuthStore()
   const userName = isLoggedIn ? user.displayName : ''
+  const location = useLocation()
+  const isDashboard = location.pathname.includes('dashboard')
 
   return (
     <header className="px-12 h-20 flex justify-between items-center backdrop-blur-sm bg-white/30 sticky top-0 z-10">
       <img src={homeLogo} alt="logo" className="h-full" />
       {isLoggedIn && (
         <>
-          <Search />
+          {isDashboard && <Search />}
           <UserInfo userName={userName} />
         </>
       )}
