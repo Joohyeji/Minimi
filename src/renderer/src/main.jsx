71c485d2b08d67388f25eb6b93f5577d93cc6a95
Joohyeji <rosyjoo1999@gmail.com>
2024-09-04 16:12:45 +0900

    ✨ Google Analytics 추가

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -3,4 +3,22 @@ import './assets/main.css'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+const googleAnalyticsId = import.meta.env.VITE_MEASUREMENT_ID
+
+if (googleAnalyticsId) {
+  const script1 = document.createElement('script')
+  script1.async = true
+  script1.src = `https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`
+  document.head.appendChild(script1)
+
+  const script2 = document.createElement('script')
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${googleAnalyticsId}');
+  `
+  document.head.appendChild(script2)
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
